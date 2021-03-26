76ffe307ab69a645c408eca08c46c4f5fc311436
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 15:11:39 +0100

    :white_check_mark: Tests for the first problem

---
+++ solutions/reverse-sort/src/main/kotlin/solutions.reverse.sort/Solution.kt
@@ -0,0 +1,11 @@
+class Solution {
+    companion object {
+        fun main(args: Array<String>) {
+
+        }
+    }
+}
+
+fun countReverseApplicationsToSortList(numbers: List<Int>): Int {
+    return 0
+}
