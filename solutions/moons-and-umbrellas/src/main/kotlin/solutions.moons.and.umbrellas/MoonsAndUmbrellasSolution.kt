42d03662650e4ff060601396aeb77377effa13e9
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 16:31:59 +0100

    :sparkles: Read input & print output for Moons and umbrellas

--- solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
+++ solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
@@ -4,8 +4,8 @@ fun main(args: Array<String>) {
     for (i in 1..casesNumber) {
         // Ignore first line of case
         readLine()
-
-        println("Case #$i: }")
+        val inputLine = readLine()!!.split(" ")
+        println("Case #$i: ${computeMuralCost(ProblemInput(inputLine[0].toInt(), inputLine[1].toInt(), inputLine[2]))}}")
     }
 }
 
