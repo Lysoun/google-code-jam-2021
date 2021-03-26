10f71c3b214e8a05a0e594efe0d8dbcb89adc094
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 15:17:44 +0100

    :sparkles: Read input & print output

--- solutions/reverse-sort/src/main/kotlin/solutions.reverse.sort/Solution.kt
+++ solutions/reverse-sort/src/main/kotlin/solutions.reverse.sort/Solution.kt
@@ -1,7 +1,14 @@
 class Solution {
     companion object {
         fun main(args: Array<String>) {
+            val casesNumber = readLine()!!.toInt()
 
+            for(i in 1..casesNumber) {
+                // Ignore first line of case
+                readLine()
+
+                println("Case #$i: ${countReverseApplicationsToSortList(readLine()!!.split(" ").map { it.toInt() })}")
+            }
         }
     }
 }
