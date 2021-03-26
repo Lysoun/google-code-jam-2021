b65acc2bc5cb020289bebc00fc2c37f0b209cb4d
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 16:04:31 +0100

    :sparkles: Reverse sort solution

--- solutions/reverse-sort/src/main/kotlin/solutions.reverse.sort/ReverseSortSolution.kt
+++ solutions/reverse-sort/src/main/kotlin/solutions.reverse.sort/ReverseSortSolution.kt
@@ -10,5 +10,29 @@ fun main(args: Array<String>) {
 }
 
 fun computeReverseCostToSortList(numbers: List<Int>): Int {
-    return 0
+    var totalCost = 0
+    var currentList = numbers
+    for (i in 0 until (numbers.size - 1) ) {
+        val result = reverseAndComputeCost(currentList, i)
+        currentList = result.first
+        totalCost += result.second
+    }
+    return totalCost
+}
+
+fun reverseAndComputeCost(numbers: List<Int>, startingIndex: Int): Pair<List<Int>, Int> {
+    val smallestNumberPosition = numbers.subList(startingIndex, numbers.size)
+        .mapIndexed { index, i -> index to i }
+        .minByOrNull { it.second }!!
+        .first
+
+    if (smallestNumberPosition == 0) {
+        return numbers to 1
+    }
+
+    return (
+            numbers.subList(0, startingIndex) +
+                    numbers.subList(startingIndex, smallestNumberPosition + startingIndex + 1).reversed() +
+                    numbers.subList(smallestNumberPosition + 1, numbers.size)
+            ) to (smallestNumberPosition + 1)
 }
