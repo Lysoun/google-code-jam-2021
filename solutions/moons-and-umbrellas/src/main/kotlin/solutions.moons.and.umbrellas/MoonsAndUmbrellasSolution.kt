010cdbd14cff3c62caa42d4a222525fb34fe4bf6
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 17:23:26 +0100

    :sparkles: Compute mural cost for test sets 1 & 2

--- solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
+++ solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
@@ -1,3 +1,5 @@
+import kotlin.math.min
+
 fun main(args: Array<String>) {
     val casesNumber = readLine()!!.toInt()
 
@@ -5,12 +7,48 @@ fun main(args: Array<String>) {
         // Ignore first line of case
         readLine()
         val inputLine = readLine()!!.split(" ")
-        println("Case #$i: ${computeMuralCost(ProblemInput(inputLine[0].toInt(), inputLine[1].toInt(), inputLine[2]))}}")
+        println("Case #$i: ${computeMuralCost(ProblemInput(
+            inputLine[0].toInt(),
+            inputLine[1].toInt(),
+            inputLine[2]
+        ))}}")
     }
 }
 
 fun computeMuralCost(problemInput: ProblemInput): Int {
-    return 0
+    var totalCost = 0
+    var currentState = State(problemInput.mural[0], false)
+    for(drawing in problemInput.mural.subSequence(1, problemInput.mural.length)) {
+        val result = computeCost(currentState, drawing, problemInput.cjCost, problemInput.jcCost)
+        currentState = result.first
+        totalCost += result.second
+    }
+
+    return totalCost
+}
+
+fun computeCost(state: State, drawing: Char, cjCost: Int, jcCost: Int): Pair<State, Int>  {
+    if (state.lastDrawing == '?') {
+        return State(drawing, false) to 0
+    }
+
+    if(drawing == '?') {
+        return State(state.lastDrawing, true) to 0
+    }
+
+    val newState = State(drawing, false)
+
+    if(drawing == state.lastDrawing) {
+        return newState to 0
+    }
+
+    if(drawing == 'J') {
+        return newState to cjCost
+    }
+
+    return newState to jcCost
 }
 
 data class ProblemInput(val cjCost: Int, val jcCost: Int, val mural: String)
+
+data class State(val lastDrawing: Char, val spaceSinceLastDrawing: Boolean)
