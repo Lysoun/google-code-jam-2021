ea21010ea2c154b0bae24ee404948ef143a59a9c
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 21:25:49 +0100

    :sparkles: Read input & print output

--- solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
+++ solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
@@ -1,10 +1,11 @@
 fun main(args: Array<String>) {
     val casesNumber = readLine()!!.toInt()
 
-    for (i in 1..casesNumber) {
-        val lineInput = readLine()!!.split(" ")
+    // Ignore required percentage to success
+    readLine()
 
-        println("Case #$i: ")
+    for (i in 1..casesNumber) {
+        println("Case #$i: ${findCheater(List(100) { 0 }.map { readLine()!! })}")
     }
 }
 
