f5032d473fd3e3d82081c8b03a95146bc06ff3f2
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 16:30:13 +0100

    :white_check_mark: Add tests for Moons and umbrellas

--- solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
+++ solutions/moons-and-umbrellas/src/main/kotlin/solutions.moons.and.umbrellas/MoonsAndUmbrellasSolution.kt
@@ -5,34 +5,12 @@ fun main(args: Array<String>) {
         // Ignore first line of case
         readLine()
 
-        println("Case #$i: ${computeReverseCostToSortList(readLine()!!.split(" ").map { it.toInt() })}")
+        println("Case #$i: }")
     }
 }
 
-fun computeReverseCostToSortList(numbers: List<Int>): Int {
-    var totalCost = 0
-    var currentList = numbers
-    for (i in 0 until (numbers.size - 1) ) {
-        val result = reverseAndComputeCost(currentList, i)
-        currentList = result.first
-        totalCost += result.second
-    }
-    return totalCost
+fun computeMuralCost(problemInput: ProblemInput): Int {
+    return 0
 }
 
-fun reverseAndComputeCost(numbers: List<Int>, startingIndex: Int): Pair<List<Int>, Int> {
-    val smallestNumberPosition = numbers.subList(startingIndex, numbers.size)
-        .mapIndexed { index, i -> index to i }
-        .minByOrNull { it.second }!!
-        .first
-
-    if (smallestNumberPosition == 0) {
-        return numbers to 1
-    }
-
-    return (
-            numbers.subList(0, startingIndex) +
-                    numbers.subList(startingIndex, smallestNumberPosition + startingIndex + 1).reversed() +
-                    numbers.subList(smallestNumberPosition + startingIndex + 1, numbers.size)
-            ) to smallestNumberPosition + 1
-}
+data class ProblemInput(val cjCost: Int, val jcCost: Int, val mural: String)
--- solutions/moons-and-umbrellas/src/test/kotlin/solutions/moons/and/umbrellas/MoonsAndUmbrellasSolutionTest.kt
+++ solutions/moons-and-umbrellas/src/test/kotlin/solutions/moons/and/umbrellas/MoonsAndUmbrellasSolutionTest.kt
@@ -1,6 +1,7 @@
-package solutions.reverse.sort
+package solutions.moons.and.umbrellas
 
-import computeReverseCostToSortList
+import ProblemInput
+import computeMuralCost
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
 import strikt.api.expectThat
@@ -10,15 +11,17 @@ internal class MoonsAndUmbrellasSolutionTest {
     @TestFactory
     fun `given numbers, should count number of applications of the Reverse function in order to sort the list`() =
         listOf(
-            listOf(4, 2, 1, 3) to 6,
-            listOf(1, 2) to 1,
-            listOf(7, 6, 5, 4, 3, 2, 1) to 12
-        ).map { (numbers, expected) ->
+            ProblemInput(2, 3, "CJ?CC?") to 5,
+            ProblemInput(4, 2, "CJCJ") to 10,
+            ProblemInput(1, 3, "C?J") to 1,
+            ProblemInput(2, 5, "??J???") to 0,
+            ProblemInput(2, -5, "??JJ??") to -8
+        ).map { (problemInput, expected) ->
             dynamicTest(
-                "given \"$numbers\", " +
+                "given \"$problemInput\", " +
                         "should count number of applications (\"$expected\") of the Reverse function in order to sort the list"
             ) {
-                expectThat(computeReverseCostToSortList(numbers)).isEqualTo(expected)
+                expectThat(computeMuralCost(problemInput)).isEqualTo(expected)
             }
         }
 }
