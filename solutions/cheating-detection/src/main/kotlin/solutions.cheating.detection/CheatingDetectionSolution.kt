5dd98a4fbdd719172a8b3f22ec24c7404723671a
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 21:21:05 +0100

    :white_check_mark: Add test corresponding to sample

--- solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
+++ solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
@@ -4,65 +4,10 @@ fun main(args: Array<String>) {
     for (i in 1..casesNumber) {
         val lineInput = readLine()!!.split(" ")
 
-        println("Case #$i: ${findListMatchingConstraints(ProblemInput(lineInput[0].toInt(), lineInput[1].toInt()))}")
+        println("Case #$i: ")
     }
 }
 
-const val IMPOSSIBLE = "IMPOSSIBLE"
-
-fun buildSmallestNumberPositionPerIteration(problemInput: ProblemInput): List<Int> {
-    val smallestNumberPositionPerIteration = MutableList(problemInput.listSize - 1) { 1 }
-
-    var cost = problemInput.cost - problemInput.listSize + 1
-    var i = problemInput.listSize - 1
-
-    while(cost > 0 && i > 0) {
-        if (cost >= i) {
-            cost -= i
-            smallestNumberPositionPerIteration[problemInput.listSize - i - 1] = i + 1
-        }
-        --i
-    }
-
-    return smallestNumberPositionPerIteration
+fun findCheater(playersAnswers: List<String>): Int {
+    return 0
 }
-
-fun reverseSubList(list: List<Int>, startingIndex: Int, endingIndex: Int): List<Int> {
-    return list.subList(0, startingIndex) +
-            list.subList(startingIndex, endingIndex).reversed() +
-            list.subList(endingIndex, list.size)
-}
-
-fun buildListMatchingConstraints(problemInput: ProblemInput): List<Int> {
-    val smallestNumberPositionPerIteration = buildSmallestNumberPositionPerIteration(problemInput).reversed()
-    var listMatchingConstraints = List(problemInput.listSize) { it + 1 }
-
-    var index = problemInput.listSize - 2
-
-    for(i in smallestNumberPositionPerIteration) {
-        if (i > 1) {
-            listMatchingConstraints = reverseSubList(listMatchingConstraints, index, index + i)
-        }
-        --index
-    }
-
-    return listMatchingConstraints
-}
-
-fun findListMatchingConstraints(problemInput: ProblemInput): String {
-    // Having a cost inferior to listSize - 1 is impossible because there will be listSize - 1 iterations
-    // and each iteration has a minimum cost of 1
-    if (problemInput.cost < problemInput.listSize - 1) {
-        return IMPOSSIBLE
-    }
-
-    // Having a cost superior to listSize(listSize + 1)/2 - 1 is impossible because
-    // there will be listSize - 1 iterations and each iteration i has a maximum cost of listSize - i
-    if (problemInput.cost > problemInput.listSize * (problemInput.listSize + 1) / 2 - 1) {
-        return IMPOSSIBLE
-    }
-
-    return buildListMatchingConstraints(problemInput).joinToString(" ")
-}
-
-data class ProblemInput(val listSize: Int, val cost: Int)
--- solutions/cheating-detection/src/test/kotlin/solutions/cheating/detection/CheatingDetectionSolutionTest.kt
+++ solutions/cheating-detection/src/test/kotlin/solutions/cheating/detection/CheatingDetectionSolutionTest.kt
@@ -1,27 +1,21 @@
-package solutions.reverse.sort.engineering
+package solutions.cheating.detection
 
-import ProblemInput
-import findListMatchingConstraints
-import org.junit.jupiter.api.DynamicTest.dynamicTest
-import org.junit.jupiter.api.TestFactory
+import findCheater
+import org.junit.jupiter.api.Test
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import java.io.File
 
 internal class CheatingDetectionSolutionTest {
-    @TestFactory
-    fun `given problem input, should give a list of numbers that corresponds to constraints or IMPOSSIBLE, if no such list exists`() =
-        listOf(
-            ProblemInput(4, 6) to "4 3 2 1",
-            ProblemInput(2, 1) to "1 2",
-            ProblemInput(7, 12) to "7 6 5 4 3 2 1",
-            ProblemInput(7, 2) to "IMPOSSIBLE",
-            ProblemInput(2, 1000) to "IMPOSSIBLE",
-        ).map { (problemInput, expected) ->
-            dynamicTest(
-                "given \"$problemInput\", " +
-                        "should give a list of numbers that corresponds to constraints or IMPOSSIBLE, if no such list exists"
-            ) {
-                expectThat(findListMatchingConstraints(problemInput)).isEqualTo(expected)
-            }
-        }
+    @Test
+    fun `given input sample, should find that cheater is 59`() {
+        // Given
+        val playersAnswers = File("src/test/resources/input_sample.txt").readLines().drop(2)
+
+        // When
+        val cheater = findCheater(playersAnswers)
+
+        // Then
+        expectThat(cheater).isEqualTo(59)
+    }
 }
