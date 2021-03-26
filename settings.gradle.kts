316c0a1b2a081e7cbbf63b5f60e50a640e394683
Élise Rubio <elise.rubio@4sh.fr>
2021-03-26 21:08:41 +0100

    :tada: Initialize cheating detection solution

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,6 @@ rootProject.name = "google-code-jam-2021-solutions"
 include(
     "solutions:reverse-sort",
     "solutions:moons-and-umbrellas",
-    "solutions:reverse-sort-engineering"
+    "solutions:reverse-sort-engineering",
+    "solutions:cheating-detection"
 )
---
+++ solutions/cheating-detection/src/test/kotlin/solutions/cheating/detection/CheatingDetectionSolutionTest.kt
@@ -0,0 +1,27 @@
+package solutions.reverse.sort.engineering
+
+import ProblemInput
+import findListMatchingConstraints
+import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.TestFactory
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+internal class CheatingDetectionSolutionTest {
+    @TestFactory
+    fun `given problem input, should give a list of numbers that corresponds to constraints or IMPOSSIBLE, if no such list exists`() =
+        listOf(
+            ProblemInput(4, 6) to "4 3 2 1",
+            ProblemInput(2, 1) to "1 2",
+            ProblemInput(7, 12) to "7 6 5 4 3 2 1",
+            ProblemInput(7, 2) to "IMPOSSIBLE",
+            ProblemInput(2, 1000) to "IMPOSSIBLE",
+        ).map { (problemInput, expected) ->
+            dynamicTest(
+                "given \"$problemInput\", " +
+                        "should give a list of numbers that corresponds to constraints or IMPOSSIBLE, if no such list exists"
+            ) {
+                expectThat(findListMatchingConstraints(problemInput)).isEqualTo(expected)
+            }
+        }
+}
