679c251af6ac6f8271747ef10e1d3c230565743a
Élise Rubio <elise.rubio@4sh.fr>
2021-03-27 19:33:57 +0100

    :white_check_mark: Add test cases

--- solutions/qualifications/median-sort/src/test/kotlin/solutions/median/sort/MedianSortSolutionTest.kt
+++ solutions/qualifications/median-sort/src/test/kotlin/solutions/median/sort/MedianSortSolutionTest.kt
@@ -8,14 +8,14 @@ import strikt.assertions.isTrue
 
 internal class MedianSortSolutionTest {
     @ParameterizedTest
-    @ValueSource(strings = ["5 4 3 2 1", "1 3 5 4 2"])
+    @ValueSource(strings = ["5 4 3 2 1", "1 3 5 4 2", "1 2 3 4 5 6", "4 5 3 2 6 1"])
     fun `given `(input: String) {
         // Given
         val goodAnswer = input.split(" ").map { it.toInt() }
         val judge = TestJudge(goodAnswer)
 
         // When
-        val answer = sortList(5, judge)
+        val answer = sortList(goodAnswer.size, judge)
         val isAnswerCorrect = answer == goodAnswer || answer == goodAnswer.reversed()
 
         // Then
