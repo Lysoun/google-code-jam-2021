40f26c9a6f72ef777ea6078f9a481a34163c2b33
Élise Rubio <elise.rubio@4sh.fr>
2021-03-27 14:58:33 +0100

    :sparkles: Solve problem using naive implementation (taking the one with the most good answers)

--- solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
+++ solutions/cheating-detection/src/main/kotlin/solutions.cheating.detection/CheatingDetectionSolution.kt
@@ -9,6 +9,26 @@ fun main(args: Array<String>) {
     }
 }
 
+fun numberOfGoodAnswersToSkillOrDifficulty(numberOfGoodAnswers: Int, totalNumber: Int): Double {
+    return (6.0 * numberOfGoodAnswers / totalNumber) - 3
+}
+
+data class Player(val number: Int, val answers: List<Int>, val numberOfGoodAnswers: Int, val skill: Double) {
+    companion object {
+        fun from(number: Int, playerAnswers: String): Player {
+            val playersAnswersAsShorts = playerAnswers.toList()
+                .map { answer -> Character.getNumericValue(answer) }
+            val numberOfGoodAnswers = playersAnswersAsShorts.sum()
+
+            val skill = numberOfGoodAnswersToSkillOrDifficulty(numberOfGoodAnswers, 10000)
+
+            return Player(number, playersAnswersAsShorts, numberOfGoodAnswers, skill)
+        }
+    }
+}
+
 fun findCheater(playersAnswers: List<String>): Int {
-    return 0
+    val players = playersAnswers.mapIndexed { index, answers -> Player.from(index + 1, answers) }
+
+    return players.maxByOrNull { it.numberOfGoodAnswers }!!.number
 }
