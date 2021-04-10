050e7819bce793f9c58e9f775620626e51da81e3
Élise Rubio <elise.rubio@4sh.fr>
2021-04-10 17:46:53 +0200

    :sparkles: Handle succession of one number

--- solutions/round1/A/append-sort/src/main/kotlin/append/sort/AppendSortSolution.kt
+++ solutions/round1/A/append-sort/src/main/kotlin/append/sort/AppendSortSolution.kt
@@ -11,12 +11,23 @@ fun main(args: Array<String>) {
 fun countDigitsRequiredForAppendSort(list: List<Int>): Int {
     val sortedList = list.toMutableList()
     var digitsRequired = 0
+    var digitToAdd = 0
+    var last = sortedList[0]
 
     for(i in 1 until list.size) {
-        while(sortedList[i] <= sortedList[i - 1]) {
-            if(sortedList[i] == sortedList[i - 1])
-            sortedList[i] = sortedList[i] * 10 + 9
+        if (sortedList[i] == last) {
+            ++digitToAdd
+        } else {
+            last = sortedList[i]
+        }
+
+        while (sortedList[i] <= sortedList[i - 1]) {
+            sortedList[i] = sortedList[i] * 10 + digitToAdd
             ++digitsRequired
+        }
+
+        if (digitToAdd > 9) {
+            digitToAdd = 0
         }
     }
 
