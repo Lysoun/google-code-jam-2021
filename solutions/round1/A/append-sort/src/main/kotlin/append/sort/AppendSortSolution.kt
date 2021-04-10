1888f0866b166ae4921a1fe53f2348c4982b5fe7
Élise Rubio <elise.rubio@4sh.fr>
2021-04-10 20:59:21 +0200

    :sparkles: Test set 2!!

--- solutions/round1/A/append-sort/src/main/kotlin/append/sort/AppendSortSolution.kt
+++ solutions/round1/A/append-sort/src/main/kotlin/append/sort/AppendSortSolution.kt
@@ -1,51 +1,68 @@
+import java.math.BigInteger
+
 fun main(args: Array<String>) {
     val casesNumber = readLine()!!.toInt()
 
     for (i in 1..casesNumber) {
         // Ignore list size
         readLine()
-        println("Case #$i: ${countDigitsRequiredForAppendSort(readLine()!!.split(" ").map { it.toInt() })}")
+        println("Case #$i: ${countDigitsRequiredForAppendSort(readLine()!!.split(" ").map { it.toBigInteger() })}")
     }
 }
 
-fun countDigitsRequiredForAppendSort(list: List<Int>): Int {
-    val sortedList = list.toMutableList()
+fun countDigitsRequiredForAppendSort(list: List<BigInteger>): Int {
     var digitsRequired = 0
-    var digitsToAdd = ""
-    var last = sortedList[0]
+    var digitsToAdd: String
+    val sortedList = mutableListOf(list[0])
+    var last = list[0]
 
-    for(i in 1 until list.size) {
-        var elt = sortedList[i]
-
-        if (elt != last) {
-            last = elt
-            digitsToAdd = ""
-        }
-
-        while (elt <= sortedList[i - 1]) {
-            elt = (sortedList[i].toString() + digitsToAdd).toInt()
-
-            if (elt <= sortedList[i - 1]) {
-                digitsToAdd = if (digitsToAdd == "") {
-                    "0"
-                } else {
-                    if (digitsToAdd[digitsToAdd.length - 1] == '9') {
-                        digitsToAdd.substring(0, digitsToAdd.length - 1) + "00"
-                    } else {
-                        digitsToAdd.substring(
-                            0,
-                            digitsToAdd.length - 1
-                        ) + (Character.getNumericValue(digitsToAdd[digitsToAdd.length - 1]) + 1).toString()
-                    }
-                }
-            }
-        }
-
-        if (elt != sortedList[i]) {
-            digitsRequired += digitsToAdd.length
-            sortedList[i] = elt
-        }
+    for (i in 1 until list.size) {
+        var elt = list[i]
+        digitsToAdd = diff(last, elt)
+        elt = (elt.toString() + digitsToAdd).toBigInteger()
+        digitsRequired += digitsToAdd.length
+        last = elt
+        sortedList.add(last)
     }
 
     return digitsRequired
+}
+
+fun diff(int1: BigInteger, int2: BigInteger): String {
+    if (int1 < int2) {
+        return ""
+    }
+
+    if(int1 == int2) {
+        return "0"
+    }
+
+    val str1 = int1.toString()
+    val str2 = int2.toString()
+
+    val lengthDifference = str1.length - str2.length
+    val startOfStr1 = str1.substring(0, str2.length).toBigInteger()
+
+    if (startOfStr1 > int2) {
+        return "0".repeat(lengthDifference + 1)
+    }
+
+    if (startOfStr1 < int2) {
+        return "0".repeat(lengthDifference)
+    }
+
+    return incrementDigitsToAdd(str1.substring(str2.length, str1.length))
+}
+
+fun incrementDigitsToAdd(digitsToAdd: String): String {
+    return if (digitsToAdd == "") {
+        "0"
+    } else {
+        if (digitsToAdd.all { it == '9' }) {
+            "0".repeat(digitsToAdd.length + 1)
+        } else {
+            val result = (digitsToAdd.toBigInteger() + BigInteger.ONE).toString()
+            "0".repeat(digitsToAdd.length - result.length) + result
+        }
+    }
 }
--- solutions/round1/A/append-sort/src/test/kotlin/solutions/append/sort/AppendSortSolutionTest.kt
+++ solutions/round1/A/append-sort/src/test/kotlin/solutions/append/sort/AppendSortSolutionTest.kt
@@ -10,18 +10,24 @@ internal class AppendSortSolutionTest {
     @TestFactory
     fun `given numbers, should count number of digits to add for list to be sorted`() =
         listOf(
+            listOf(1000000, 1) to 6,
+            listOf(1000009, 1000008, 1000007, 1000006) to 6,
+            List(100) { 1 } to 188,
             listOf(100, 7, 10) to 4,
             listOf(10, 10) to 1,
             listOf(4, 19, 1) to 2,
             listOf(1, 2, 3) to 0,
             listOf(10, 10, 10) to 2,
-            listOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) to 12
+            listOf(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) to 12,
+            listOf(70, 1) to 2,
+            listOf(10, 7) to 1,
+            listOf(1000, 100, 10) to 3
         ).map { (numbers, expected) ->
             dynamicTest(
                 "given \"$numbers\", " +
                         "should count number of digits (\"$expected\") for list to be sorted"
             ) {
-                expectThat(countDigitsRequiredForAppendSort(numbers)).isEqualTo(expected)
+                expectThat(countDigitsRequiredForAppendSort(numbers.map { it.toBigInteger() })).isEqualTo(expected)
             }
         }
 }
