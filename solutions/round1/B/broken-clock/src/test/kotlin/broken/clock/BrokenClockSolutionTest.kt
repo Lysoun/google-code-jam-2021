3f2ef314881224657020aac26313ca34173dddd2
Ryunos <lisouni2@gmail.com>
2021-04-25 22:00:49 +0200

    :construction: Trying problem...

--- solutions/round1/B/broken-clock/src/test/kotlin/broken/clock/BrokenClockSolutionTest.kt
+++ solutions/round1/B/broken-clock/src/test/kotlin/broken/clock/BrokenClockSolutionTest.kt
@@ -15,18 +15,19 @@ internal class BrokenClockSolutionTest {
             listOf(0, 0, 0) to Time(0, 0, 0, 0),
             listOf(0, 21600000000000, 23400000000000) to Time(6, 30, 0, 0),
             listOf(1476000000000, 2160000000000, 3723000000000) to Time(1, 2, 3, 0),
+                listOf(0, 7200000000000, 10800000000000) to Time(2, 0, 0, 0),
 
                 listOf(5400000000000, 5400000000000, 5400000000000) to Time(0, 0, 0, 0),
                 listOf(10800000000000, 32400000000000, 32400000000000) to Time(0, 30, 0, 0),
                 listOf(23076000000000, 23076000000000, 25323000000000) to Time(1, 2, 3, 0),
 
-                listOf(0, 11, 179) to Time(0, 0, 0, 1),
+                listOf(0, 11, 179) to Time(0, 0, 0, 1)
         ).map { (numbers, expected) ->
             dynamicTest(
                 "given the clock angles \"$numbers\", " +
                         "should compute the time (\"$expected\")"
             ) {
-                expectThat(computeTime(numbers as List<BigInteger>)).isEqualTo(expected)
+                expectThat(computeTime(numbers.map { BigInteger.valueOf(it.toLong()) })).isEqualTo(expected)
             }
         }
 }
