98d39fcab3c8898bfdc2c6875006edcf759feddd
Élise Rubio <elise.rubio@4sh.fr>
2021-05-01 15:35:08 +0200

    :sparkles: Pass test set 1

--- solutions/round1/C/roaring-year/src/main/kotlin/roaring/year/RoaringYearSolution.kt
+++ solutions/round1/C/roaring-year/src/main/kotlin/roaring/year/RoaringYearSolution.kt
@@ -9,5 +9,61 @@ fun main(args: Array<String>) {
 }
 
 fun findNextRoaringYear(currentYear: String): BigInteger {
-    return BigInteger.ZERO
+    val roaringYears = MutableList<BigInteger>(0) { BigInteger.ZERO }
+    val currentYearBigInt = BigInteger(currentYear)
+
+    var firstTry = true
+    for (i in currentYear.indices) {
+        var foundMinimum = false
+        var k = BigInteger.ZERO
+        while (!foundMinimum) {
+            val firstNumber = if (firstTry) {
+                BigInteger.ONE
+            } else {
+                (BigInteger(currentYear.substring(0, i + 1)) + k)
+            }
+
+            var j = 1L
+            var year = firstNumber.toString()
+            if (year.length <= (currentYear.length + 1) / 2 || roaringYears.isEmpty()) {
+                var yearBigInt: BigInteger
+                var yearTooBig = false
+
+                do {
+                    val newYear = (year + (firstNumber + BigInteger.valueOf(j)).toString())
+                    year = newYear
+                    yearBigInt = BigInteger(newYear)
+
+                    if (year.length > currentYear.length + 2) {
+                        yearTooBig = true
+                    } else {
+                        ++j
+                    }
+                } while (yearBigInt <= currentYearBigInt && !yearTooBig)
+
+                val currentMin = roaringYears.minOrNull()
+                if (yearBigInt > currentYearBigInt && (currentMin == null || yearBigInt < currentMin) && year.length > 1) {
+                    if (k >= BigInteger.ONE && !firstTry) {
+                        foundMinimum = true
+                    }
+                    roaringYears.add(yearBigInt)
+                } else {
+                    if (yearTooBig && k > BigInteger.ONE && !firstTry) {
+                        foundMinimum = true
+                    } else {
+                        if(!firstTry) {
+                            ++k
+                        }
+                    }
+                }
+            } else {
+                if (!firstTry) {
+                    foundMinimum = true
+                }
+            }
+            firstTry = false
+        }
+    }
+
+    return roaringYears.minOrNull()!!
 }
--- solutions/round1/C/roaring-year/src/test/kotlin/solutions/roaring/year/RoaringYearSolutionTest.kt
+++ solutions/round1/C/roaring-year/src/test/kotlin/solutions/roaring/year/RoaringYearSolutionTest.kt
@@ -2,9 +2,11 @@ package solutions.roaring.year
 
 import findNextRoaringYear
 import org.junit.jupiter.api.DynamicTest.dynamicTest
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
 import strikt.api.expectThat
 import strikt.assertions.isEqualTo
+import java.io.File
 import java.math.BigInteger
 
 internal class RoaringYearSolutionTest {
@@ -14,7 +16,29 @@ internal class RoaringYearSolutionTest {
             "2020" to 2021,
             "2021" to 2122,
             "68000" to 78910,
-            "101" to 123
+            "101" to 123,
+            "9999999" to 10001001,
+            "1234566" to 1234567,
+            "789" to 910,
+            "1" to 12,
+            "9" to 12,
+            "99" to 123,
+            "67" to 78,
+            "123" to 234,
+            "1999" to 2021,
+            "10001000" to 10001001,
+            "20212021" to 20212022,
+            "9898000" to 9899100,
+            "333333" to 333334,
+            "333434" to 333435,
+            "33333" to 34567,
+            "1000000" to 1234567,
+            "100000" to 100101,
+            "123124" to 123456,
+            "123456" to 124125,
+            "199200" to 200201,
+            "998999" to 1234567
+
         ).map { (currentYear, nextRoaringYear) ->
             dynamicTest(
                 "given current year, \"$currentYear\", " +
@@ -23,4 +47,24 @@ internal class RoaringYearSolutionTest {
                 expectThat(findNextRoaringYear(currentYear)).isEqualTo(BigInteger.valueOf(nextRoaringYear.toLong()))
             }
         }
+
+    @Test
+    fun `run case 1`() {
+        val testFileInput = File("./src/test/resources/ts1_input.txt").bufferedReader()
+        val testFileOutput = File("./src/test/resources/ts1_output.txt").bufferedReader()
+
+
+        val nbOfCase = testFileInput.readLine()!!.toInt()
+
+        for (i in 0 until nbOfCase) {
+            val input = testFileInput.readLine()!!
+            val output = BigInteger(testFileOutput.readLine()!!.split(" ")[2])
+            val solution = findNextRoaringYear(input)
+
+            println("Case #${i + 1}: input: $input solution: $solution expected: $output")
+            expectThat(solution).isEqualTo(output)
+
+        }
+
+    }
 }
